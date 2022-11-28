9f8511b14e4dbf53c58559eb4d74d09cc687d4b7
Ruan Pretorius <ruan@Ruans-MacBook-Pro.local>
2022-11-28 16:21:15 +0200

    added row count data quality checker

--- src/main.py
+++ src/main.py
@@ -1,23 +1,61 @@
-from fastapi import FastAPI
-from src.utils import Greetings
+import pandas as pd
+from fastapi import FastAPI, HTTPException
+from src.utils import DataQualityChecker
 
 app = FastAPI()
-my_greeting = Greetings()
+dqc = DataQualityChecker()
+
+DOCSTRING_EXAMPLE_PAYLOAD = """
+{
+    "a": [1,2,3,4],
+    "b": [5,6,7,8]
+}
+"""
 
 
 @app.get("/")
 async def get_greeting():
-    """
-    Return greeting message in the following format:
-    {"greeting": "<greeting>"}
-    """
-    return {"message": my_greeting.get_greeting()}
+    """Welcome screen."""
+    return {"about": "Welcome to the data quality checker. Go to the /docs endpoint for help."}
 
-@app.post("/set-greeting")
-def set_greeting(payload:dict):
+@app.post("/quality-check/min-rows")
+def min_rows(min_rows:int = 0, payload:dict = {}):
     """
-    Set new greeting message.
-    Payload must be in the following format:
-    {"greeting": "<new-greeting>"}
+    Check valid row count (more than minimum).
+
+    Example payload:
+    {
+        "a": [1,2,3,4],
+        "b": [5,6,7,8]
+    }
     """
-    my_greeting.set_greeting(payload["greeting"])
+    # turn payload into pandas df
+    try:
+        df = pd.DataFrame(payload)
+    except Exception as ex:
+        raise HTTPException(status_code=201, detail=f"Could not convert payload to pandas DataFrame.")
+    
+    # check row quality
+    validity = dqc.has_at_least_rows(df, min_rows=min_rows)
+    return {"is_valid": validity}
+
+@app.post("/quality-check/row-range")
+def row_range(min_rows:int = 0, max_rows:int=9999, payload:dict = {}):
+    """
+    Check valid row count (within given range).
+
+    Example payload:
+    {
+        "a": [1,2,3,4],
+        "b": [5,6,7,8]
+    }
+    """
+    # turn payload into pandas df
+    try:
+        df = pd.DataFrame(payload)
+    except Exception as ex:
+        raise HTTPException(status_code=201, detail=f"Could not convert payload to pandas DataFrame")
+    
+    # check row quality
+    validity = dqc.has_valid_row_range(df, min_rows=min_rows, max_rows=max_rows)
+    return {"is_valid": validity}
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,44 @@
-class Greetings():
-    def __init__(self, greeting:str = "Hello World") -> None:
-        self.greeting = greeting
+import pandas as pd
 
-    def get_greeting(self) -> None:
-        return self.greeting
-    
-    def set_greeting(self, new_greeting:str) -> None:
-        self.greeting = new_greeting
+class DataQualityChecker():
+    """
+    Class that performs various data quality checks 
+    on pandas DataFrames.
+    """
+    def __init__(self) -> None:
+        pass
+
+    def has_at_least_rows(self, df:pd.DataFrame, min_rows:int) -> bool:
+        """
+        Check wether the given data has at least the 
+        minimum row count.
+
+        Args:
+            df (df:pd.DataFrame): Data to check for valid row count.
+            min_rows (int): Minimum accepted number of rows.
+
+        Return:
+            bool: Whether the data has a valid row count or not.
+        """
+        if df.shape[0] >= min_rows:
+            return True
+        else:
+            return False
+
+    def has_valid_row_range(self, df:pd.DataFrame, min_rows:int, max_rows:int) -> bool:
+        """
+        Check wether the given data has a row count between
+        the minimum and maximum allowed.
+
+        Args:
+            df (df:pd.DataFrame): Data to check for valid row count.
+            min_rows (int): Minimum accepted number of rows.
+            max_rows (int): Maximum accepted number of rows.
+
+        Return:
+            bool: Whether the data has a valid row count or not.
+        """
+        if (df.shape[0] >= min_rows) & (df.shape[0] <= max_rows):
+            return True
+        else:
+            return False
